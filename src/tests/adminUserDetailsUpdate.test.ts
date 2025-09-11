12caad6e2471ef48f0b5b96746b61f8a7290fb64
Nandini <z5593045@ad.unsw.edu.au>
2025-09-11 23:09:30 +1000

    created test file for userupdate and outlined file

---
+++ src/tests/adminUserDetailsUpdate.test.ts
@@ -0,0 +1,11 @@
+import { adminUserDetailsUpdate } from "../auth/adminUserDetailsUpdate";
+
+describe("", () => {
+    describe("Error Cases", () => {
+        test("", () => {});
+    });
+
+    describe("Success Cases", () => {
+        test("", () => {});
+    });
+});
